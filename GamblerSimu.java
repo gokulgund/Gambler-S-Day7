98a401192d3516b3d0aa2c11e9bc564dc5e57bce
gokul gund <gokulgund15@gmail.com>
2021-12-10 09:31:01 +0530

    Added : Program For Show Stake and Bet

--- GamblerSimu.java
+++ GamblerSimu.java
@@ -1,8 +1,16 @@
 package com.bl.gambler;
 
 public class GamblerSimu {
+	
+	public static final int STAKE = 100;
+	public static final int BET = 1;
+	
+	public static void print() {
+		System.out.println("Gambler Have Invest $"+STAKE+" Everyday");
+		System.out.println("Gamber Bet $"+BET+" On Every Game");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambler Simulator...!");
+		print();
 	}
-
 }
