65f2a99da6500988d4e6254f830137f2a6b16fff
gokul gund <gokulgund15@gmail.com>
2021-12-10 18:43:23 +0530

    Added : Program For the Gambler 50% Win Or Lost then resign day

--- GamblerSimu.java
+++ GamblerSimu.java
@@ -2,14 +2,13 @@ package com.bl.gambler;
 
 public class GamblerSimu {
 
-	public static final int STAKE = 100;
-	public static final int BET = 1;
+	public static int STAKE = 100;
+	public static int BET = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambler Simulator...!");
 		print();
-		String result = winOrLose();
-		System.out.println("You Have : " + result);
+		dayResign();
 	}
 
 	public static void print() {
@@ -17,14 +16,18 @@ public class GamblerSimu {
 		System.out.println("Gamber Bet $" + BET + " On Every Game.");
 	}
 
-	public static String winOrLose() {
-		double betcheck = Math.floor(Math.random() * 10) % 2;
-		if (betcheck == 1) {
-			return "win";
-		} else {
-			return "Lose";
+	public static void dayResign() {
+		while (STAKE != 50 && STAKE != 150) {
+			double betCheck = Math.floor(Math.random() * 10) % 2;
+			if (betCheck == 1) {
+				STAKE = STAKE + BET;
+				System.out.println("Gambler won !");
+			} else {
+				STAKE = STAKE - BET;
+				System.out.println("oops ! Gambler lost");
+			}
+			System.out.println("Stake value is: " + STAKE);
 		}
-
+		System.out.println("....Now Resign For The Day....");
 	}
-
 }
