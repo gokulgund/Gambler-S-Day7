c77a89ba6ed25fb8840c5582f69341eacce0abc4
gokul gund <gokulgund15@gmail.com>
2021-12-10 23:31:46 +0530

    Added : Program for the Number of days win & lost in month & how Much

--- GamblerSimu.java
+++ GamblerSimu.java
@@ -21,25 +21,34 @@ public class GamblerSimu {
 		int Lost = 0;
 		int TotalWin = 0;
 		int TotalLost = 0;
-		
-		for (int day = 1; day <= 20; day++) {
+		int numOfDaysWin = 0;
+		int numOfDaysLost = 0;
 
-			while (STAKE != 50 && STAKE != 150) {
-				double betCheck = Math.floor(Math.random() * 10) % 2;
-				if (betCheck == 1) {
-					STAKE = STAKE + BET;
-				} else {
-					STAKE = STAKE - BET;
-				}
+		for (int day = 1; day <= 30; day++) {
+			double betCheck = Math.floor(Math.random() * 10) % 2;
+			
+			if (betCheck == 1) {
+				STAKE = STAKE + BET;
+				numOfDaysWin++;
+			} else {
+				STAKE = STAKE - BET;
+				numOfDaysLost++;
 			}
+			
 			Win = STAKE + 100;
 			Lost = STAKE - 100;
+			System.out.println("---------------------------------------------------");
 			System.out.println("Gambler Have Win The amount Today is : " + Win);
 			System.out.println("Gambler Have Lost the amount today is : " + Lost);
 			TotalWin = TotalWin + Win;
 			TotalLost = TotalLost + Lost;
 		}
-		System.out.println("Gamber have win the total amount in 20 day is :" + TotalWin);
-		System.out.println("Gamber have lost the total amount in 20 day is :" + TotalLost);
+		System.out.println("************************************************************");
+		System.out.println("In Month Gambler has win number of days is : " + numOfDaysWin);
+		System.out.println("In Month Gambler has lost number of days is : " + numOfDaysLost);
+		System.out.println("*************************************************************");
+		System.out.println("Gamber have win the total amount in month is : "+ TotalWin);
+		System.out.println("Gamber have lost the total amount in month is : "+ TotalLost);
 	}
+		
 }
