a102017e0da6b6b7efac9bf2deb66db5bcb5b233
gokul gund <gokulgund15@gmail.com>
2021-12-10 22:50:19 +0530

    Added : Program for the gambler 20 days playing total win and lost amount

--- GamblerSimu.java
+++ GamblerSimu.java
@@ -8,7 +8,7 @@ public class GamblerSimu {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambler Simulator...!");
 		print();
-		dayResign();
+		forDays();
 	}
 
 	public static void print() {
@@ -16,18 +16,30 @@ public class GamblerSimu {
 		System.out.println("Gamber Bet $" + BET + " On Every Game.");
 	}
 
-	public static void dayResign() {
-		while (STAKE != 50 && STAKE != 150) {
-			double betCheck = Math.floor(Math.random() * 10) % 2;
-			if (betCheck == 1) {
-				STAKE = STAKE + BET;
-				System.out.println("Gambler won !");
-			} else {
-				STAKE = STAKE - BET;
-				System.out.println("oops ! Gambler lost");
+	public static void forDays() {
+		int Win = 0;
+		int Lost = 0;
+		int TotalWin = 0;
+		int TotalLost = 0;
+		
+		for (int day = 1; day <= 20; day++) {
+
+			while (STAKE != 50 && STAKE != 150) {
+				double betCheck = Math.floor(Math.random() * 10) % 2;
+				if (betCheck == 1) {
+					STAKE = STAKE + BET;
+				} else {
+					STAKE = STAKE - BET;
+				}
 			}
-			System.out.println("Stake value is: " + STAKE);
+			Win = STAKE + 100;
+			Lost = STAKE - 100;
+			System.out.println("Gambler Have Win The amount Today is : " + Win);
+			System.out.println("Gambler Have Lost the amount today is : " + Lost);
+			TotalWin = TotalWin + Win;
+			TotalLost = TotalLost + Lost;
 		}
-		System.out.println("....Now Resign For The Day....");
+		System.out.println("Gamber have win the total amount in 20 day is :" + TotalWin);
+		System.out.println("Gamber have lost the total amount in 20 day is :" + TotalLost);
 	}
 }
